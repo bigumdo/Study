1c5d5f8dd5c61f1f2f0eb7eac3a08180acc0d47a
bigumdo <showmethe0610@gmail.com>
2024-10-20 16:17:39 +0900

    UIToolkitTransitionEvent

--- Assets/UIToolkit/Script/UIController.cs
+++ Assets/UIToolkit/Script/UIController.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -6,15 +7,18 @@ using UnityEngine.UIElements;
 
 public class UIController : MonoBehaviour
 {
-    private VisualElement _bottomContainer;
+    public string girlText;
 
+    private VisualElement _bottomContainer;
     private Button _openButton;
     private Button _closeButton;
     private VisualElement _scrim;
     private VisualElement _bottomSheet;
     private VisualElement _boy;
+    private VisualElement _girl;
+    private Label _girlText;
 
-    private void Start()
+    private void Awake()
     {
         // UIdocument에 있는 제일 위에 visualElement를 가져온다.
         var root = GetComponent<UIDocument>().rootVisualElement;
@@ -22,6 +26,10 @@ public class UIController : MonoBehaviour
         _bottomContainer = root.Q<VisualElement>("Container_Bottom");
         //Boy
         _boy = root.Q<VisualElement>("Image_Boy");
+        //Girl
+        _girl = root.Q<VisualElement>("Image_Girl");
+        //girlText
+        _girlText = root.Q<Label>("Girl_Text");
         // Button_Open라 되어 있는 button을 가져옴
         _openButton = root.Q<Button>("Button_Open");
         // Button_Close라고 되어 있는 button을 가져옴
@@ -29,38 +37,47 @@ public class UIController : MonoBehaviour
         // Scrim라고 되어 있는 VisualElement을 가져옴
         _scrim = root.Q<VisualElement>("Scrim");
         // BottomSheet라고 되어 있는 VisualElement을 가져옴
-        _bottomSheet = root.Q<VisualElement>("BottomSheet");
+        _bottomSheet = root.Q<VisualElement>("BottomSheet"); 
+    }
 
-        // visualElement에 displayer를 None으로 만들어줌
-        _bottomContainer.style.display = DisplayStyle.None;
-
+    private void OnEnable()
+    {
         //clickEvnet가 실행되면 OnOpenButtonClicked를 실행시킨다.
         _openButton.RegisterCallback<ClickEvent>(OnOpenButtonClicked);
         //clickEvnet가 실행되면 OnCloseButtonClicked를 실행시킨다.
         _closeButton.RegisterCallback<ClickEvent>(OnCloseButtonClicked);
-
         //애니메이션 바로 시작하고 한 프레임이 지난 후에 실행해야하기 때문에
-        Invoke( "AnimationBoy",1f);
+        AnimationBoy();
+        Invoke("AnimationBoy", 1f);
+        //바텀시트가 내려온 다음 그룹은 끈다.
+        _bottomSheet.RegisterCallback<TransitionEndEvent>(OnButtonSheetDown);
     }
 
-    
+    private void OnDisable()
+    {
+        _openButton.UnregisterCallback<ClickEvent>(OnOpenButtonClicked);
+        _closeButton.UnregisterCallback<ClickEvent>(OnCloseButtonClicked);
+        _bottomSheet.UnregisterCallback<TransitionEndEvent>(OnButtonSheetDown);
+    }
+
+    private void OnButtonSheetDown(TransitionEndEvent evt)
+    {
+        if(!_bottomSheet.ClassListContains("bottomsheet--up"))
+        {
+            // visualElement에 displayer를 None으로 만들어줌
+            _bottomContainer.style.display = DisplayStyle.None;
+        }
+    }
 
     private void AnimationBoy()
     {
-        _boy.RemoveFromClassList("image--boy--inair");
-    }
-
-    private void Update()
-    {
-        //클래스가 있는지
-        Debug.Log(_boy.ClassListContains("image--boy--inair"));
+        _boy.RemoveFromClassList("image--boy--inair");  
     }
 
     private void OnCloseButtonClicked(ClickEvent evt)
     {
-        // visualElement에 displayer를 None으로 만들어줌
-        _bottomContainer.style.display = DisplayStyle.None;
-        // _bottomContainer, _scrim에 styleclass를 차가한다.
+
+        // _bottomContainer, _scrim에 styleclass를 추가한다.
 
         //넣었을 때 애니메이션이 되기 위해 추가 했던 클래스를 제거하여 다시 
         _bottomSheet.RemoveFromClassList("bottomsheet--up");
@@ -73,5 +90,19 @@ public class UIController : MonoBehaviour
         _bottomContainer.style.display = DisplayStyle.Flex;
         _bottomSheet.AddToClassList("bottomsheet--up");
         _scrim.AddToClassList("scrim--fadein");
+        GirlAnimation();
+    }
+
+    private void GirlAnimation()
+    {
+        _girl.ToggleInClassList("image--girl--down");
+        _girl.RegisterCallback<TransitionEndEvent>
+            (
+                evt => _girl.ToggleInClassList("image--girl--down")
+            );
+        _girlText.text = string.Empty;
+        string m = girlText;
+        //_girlText.text에 x가 m으로 3초안에 만든다.
+        DOTween.To(() => _girlText.text, x => _girlText.text = x, m, 3f).SetEase(Ease.Linear);
     }
 }
